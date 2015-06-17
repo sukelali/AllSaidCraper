03aa01afb468b8f86eaa42f3f928d1e284c817b3
sukelali <sukelali@gmail.com>
2015-06-17 11:14:04 +0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,4 +1,5 @@
 <?
+print "Hello, coding in the cloud!";
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
